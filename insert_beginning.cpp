88c4302552979a56c8570387de94d8c230224376
manishghule88 <manishghule26@gmail.com>
2026-02-22 15:14:16 +0530

    dev: add insert_beginning file to add node at the begin

---
+++ insert_beginning.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+struct node {
+    int data;
+    struct node* next;
+};
+
+void InsertBegin( struct node** aFirst, int aNo ) {
+    struct node* newn = NULL;
+
+    newn = new struct node;
+    newn->data = aNo;
+    newn->next = NULL;
+
+    if ( *aFirst == NULL ) {
+        *aFirst = newn;
+    }
+    else {
+        newn->next = (*aFirst);
+        *aFirst = newn;
+    }
+}
+
+void Display( struct node* aFirst ) {
+    
+    while( aFirst != NULL ) {
+        cout << "| " << aFirst->data;
+        aFirst = aFirst->next;
+    }
+    cout << " |->NULL\n";
+}
+
+int main()
+{
+    struct node* head = NULL;
+
+    InsertBegin( &head, 300 );
+    InsertBegin( &head, 200 );
+    InsertBegin( &head, 100 );
+
+    Display( head );
+
+    return 0;
+}
